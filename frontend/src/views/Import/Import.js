4b18a04b94121e07611f9045053c8608c0bc415d
Komal Joshi <komal.joshi@sjsu.edu>
2022-04-09 21:33:24 -0700

    Corrected layout for import charts

--- frontend/src/views/Import/Import.js
+++ frontend/src/views/Import/Import.js
@@ -88,40 +88,46 @@ function Import(props) {
             </div>
           </div>
           &nbsp;
-          <Card>
-            <Card.Body>
-              <Row>
-                <Col md={8}>
-                  <label style={{ "font-weight": "bold" }}>Pie Chart</label>
-                  <Chart
-                    chartType="PieChart"
-                    data={graphData}
-                    width="100%"
-                    height="400px"
-                    legendToggle
-                  />
-                </Col>
-                
-              </Row>
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Body>
-              <Row>
-                <Col md={8}>
-                  <label style={{ "font-weight": "bold" }}>Sankey Chart</label>
-                  <Chart
-                    chartType="Sankey"
-                    data={graphDataSankey}
-                    width="100%"
-                    height="400px"
-                    legendToggle
-                  />
-                </Col>
-                
-              </Row>
-            </Card.Body>
-          </Card>
+          <div className='row'>
+              <div className='col'>
+                <Card>
+                    <Card.Body>
+                        <Row>
+                            <Col md={8}>
+                            <label style={{ "font-weight": "bold" }}>Pie Chart</label>
+                            <Chart
+                                chartType="PieChart"
+                                data={graphData}
+                                width="100%"
+                                height="400px"
+                                legendToggle
+                            />
+                            </Col>
+                            
+                        </Row>
+                    </Card.Body>
+                </Card>
+              </div>
+              <div className='col'>
+                    <Card>
+                        <Card.Body>
+                            <Row>
+                                <Col md={8}>
+                                <label style={{ "font-weight": "bold" }}>Sankey Chart</label>
+                                <Chart
+                                    chartType="Sankey"
+                                    data={graphDataSankey}
+                                    width="100%"
+                                    height="400px"
+                                    legendToggle
+                                />
+                                </Col>
+                                
+                            </Row>
+                        </Card.Body>
+                </Card>
+              </div>
+          </div>
         </>
       );
 }
