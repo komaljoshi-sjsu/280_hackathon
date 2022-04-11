6eec1fc82d6233cf7514323dfdd224a781d8cddd
Komal Joshi <komal.joshi@sjsu.edu>
2022-04-10 17:06:52 -0700

    added cors for cloud deployment

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,21 @@ app.use(cors());
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Cache-Control", "no-cache");
+    next();
+  });
+
 //Define all the routes
 app.use(require('./routes/macroservices'))
 app.use(require('./routes/debtservices'))
